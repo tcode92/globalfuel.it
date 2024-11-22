9ecc5485f8c3eaa3e09ca84c103e7f5da7f94ad0
holyluck <tareqbayoumi92@gmail.com>
2024-11-22 11:58:36 +0100

    Added robot.txt and sitemap.xml removed work-with-us

--- server/src/publicRouter.ts
+++ server/src/publicRouter.ts
@@ -7,10 +7,8 @@ import {
 } from "./auth/handlers";
 import { clientCreateExternalHandler } from "./client/handlers";
 import { errorReportHandler } from "./errorReport/report";
-import { createWorkWithUsHandler } from "./work-with-us/handlers";
 
 const route: FastifyPluginAsync = async (api, opts) => {
-  api.post("/work-with-us", createWorkWithUsHandler);
   api.post("/login", authLoginHandler);
   api.post("/auth/forgot-password", authForgotPasswordHandler);
   api.post("/auth/reset-password-external", authResetExternalPasswordHandler);
--- server/src/work-with-us/handlers.ts
+++
@@ -1,9 +0,0 @@
-import { CreateWorkWithUsSchema } from "@validation/workWithUs";
-import { FastifyRequest } from "fastify";
-import { createWorkWithUsService } from "./service";
-
-export const createWorkWithUsHandler = async (req: FastifyRequest) => {
-  const data = await CreateWorkWithUsSchema.parseAsync(req.body);
-  await createWorkWithUsService(data);
-  return "OK";
-};
